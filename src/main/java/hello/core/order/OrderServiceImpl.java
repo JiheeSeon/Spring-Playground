10bb24258082344eec8301eab7d113822c4057ee
JiheeSeon <starlover27@naver.com>
2021-06-18 14:28:24 +0900

    Snapshot 6.4:: Applying lombok, shorten code

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,21 +3,16 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 //  [필드 주입] -> Anti pattern
 //  @Autowired
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; // DIP, 구체적인 클래스에 대해 전혀 모름
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
